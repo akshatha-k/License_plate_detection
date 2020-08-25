4484c065ea255f4c482b9e7cf3823fa52d534ea2
Akshatha <akshutk@gmail.com>
2020-08-25 22:47:30 +0530

    added annotate tool

--- gen-outputs.py
+++ gen-outputs.py
@@ -1,60 +1,52 @@
 import sys
 import cv2
 import numpy as np
-
+from args import get_args
 from glob						import glob
 from os.path 					import splitext, basename, isfile
 from src.utils 					import crop_region, image_files_from_folder
 from src.drawing_utils			import draw_label, draw_losangle, write2img
 from src.label 					import lread, Label, readShapes
+from os import listdir
+from os.path import isfile, join
 
 from pdb import set_trace as pause
-
+args =get_args()
 
 YELLOW = (  0,255,255)
 RED    = (  0,  0,255)
 
-input_dir = sys.argv[1]
-output_dir = sys.argv[2]
+input_dir = args.input_dir
+output_dir = args.output_dir
 
-img_files = image_files_from_folder(input_dir)
 
-for img_file in img_files:
+inp_files = [f for f in listdir(input_dir) if isfile(join(input_dir, f))]
+lp_files = [f for f in listdir(output_dir) if isfile(join(output_dir, f))]
 
-	bname = splitext(basename(img_file))[0]
 
-	I = cv2.imread(img_file)
 
-	detected_cars_labels = '%s/%s_cars.txt' % (output_dir,bname)
+for i,inp_path in enumerate(inp_files):	
+	I = cv2.imread("{}/{}".format(input_dir,inp_path))
+	
+	#draw_label(I,lcar,color=YELLOW,thickness=3)
+	lp_label="{}/{}_lp.txt".format(output_dir,inp_path[:-4])
+	print(inp_path)
+	print(lp_label)
+	draw_label(I,lcar,color=YELLOW,thickness=3)
+	# if isfile("{}/{}".format(input_dir,inp_path)):
 
-	Lcar = lread(detected_cars_labels)
+	# 	Llp_shapes = readShapes(lp_label)
+	# 	pts = Llp_shapes[0].pts*lcar.wh().reshape(2,1) + lcar.tl().reshape(2,1)
+	# 	ptspx = pts*np.array(I.shape[1::-1],dtype=float).reshape(2,1)
+	# 	draw_losangle(I,ptspx,RED,3)
+	# 	'''
+	# 	if isfile(lp_label_str):
+	# 		with open(lp_label_str,'r') as f:
+	# 			lp_str = f.read().strip()
+	# 		llp = Label(0,tl=pts.min(1),br=pts.max(1))
+	# 		write2img(I,llp,lp_str)
 
-	sys.stdout.write('%s' % bname)
-
-	if Lcar:
-
-		for i,lcar in enumerate(Lcar):
-
-			draw_label(I,lcar,color=YELLOW,thickness=3)
-
-			lp_label 		= '%s/%s_%dcar_lp.txt'		% (output_dir,bname,i)
-			lp_label_str 	= '%s/%s_%dcar_lp_str.txt'	% (output_dir,bname,i)
-
-			if isfile(lp_label):
-
-				Llp_shapes = readShapes(lp_label)
-				pts = Llp_shapes[0].pts*lcar.wh().reshape(2,1) + lcar.tl().reshape(2,1)
-				ptspx = pts*np.array(I.shape[1::-1],dtype=float).reshape(2,1)
-				draw_losangle(I,ptspx,RED,3)
-
-				if isfile(lp_label_str):
-					with open(lp_label_str,'r') as f:
-						lp_str = f.read().strip()
-					llp = Label(0,tl=pts.min(1),br=pts.max(1))
-					write2img(I,llp,lp_str)
-
-					sys.stdout.write(',%s' % lp_str)
-
-	cv2.imwrite('%s/%s_output.png' % (output_dir,bname),I)
-	sys.stdout.write('\n')
+	# 		sys.stdout.write(',%s' % lp_str)
+	# 		'''
+	cv2.imwrite("{}/{}_output.png".format(output_dir,inp_path[:-4]),I)
 
--- get-networks.sh
+++ get-networks.sh
@@ -5,5 +5,5 @@ set -e
 mkdir data/lp-detector -p
 
 
-wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net_update1.h5   -P /content/alpr-unconstrained/data/lp-detector/
-wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net_update1.json -P /content/alpr-unconstrained/data/lp-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net_update1.h5   -P /content/License_plate_detection/data/lp-detector/
+wget -c -N www.inf.ufrgs.br/~smsilva/alpr-unconstrained/data/lp-detector/wpod-net_update1.json -P /content/License_plate_detection/data/lp-detector/
